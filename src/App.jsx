2ba6fee40c26c98076906c973de6477d981305fa
JasminA-97 <21mp1828@rit.ac.in>
2024-06-01 23:15:48 +0530

    Resturant listing app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Preview from './pages/Preview';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-import './App.css'
 
 function App() {
-
-
   return (
-    <>
-     
-    </>
-  )
+
+      <>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/preview" element={<Preview />} />
+        </Routes>
+        <Footer />
+      </>
+
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <>
+     <footer className="footer bg-dark text-white mt-5">
+      <Container>
+        <Row className="py-4">
+          <Col md="3">
+            <h5>Restaurant Listing</h5>
+            <ul className="list-unstyled">
+              <li><a href="/" className="text-white">Home</a></li>
+              <li><a href="/about" className="text-white">About Us</a></li>
+              <li><a href="/contact" className="text-white">Contact Us</a></li>
+            </ul>
+          </Col>
+          <Col md="3">
+            <h5>Services</h5>
+            <ul className="list-unstyled">
+              <li><a href="/restaurants" className="text-white">Find Restaurants</a></li>
+              <li><a href="/reviews" className="text-white">Read Reviews</a></li>
+              <li><a href="/signup" className="text-white">Sign Up</a></li>
+            </ul>
+          </Col>
+          <Col md="3">
+            <h5>Legal</h5>
+            <ul className="list-unstyled">
+              <li><a href="/privacy" className="text-white">Privacy Policy</a></li>
+              <li><a href="/terms" className="text-white">Terms of Service</a></li>
+            </ul>
+          </Col>
+          <Col md="3">
+            <h5>Follow Us</h5>
+            <ul className="list-unstyled">
+              <li><a href="https://www.facebook.com" className="text-white">Facebook</a></li>
+              <li><a href="https://www.twitter.com" className="text-white">Twitter</a></li>
+              <li><a href="https://www.instagram.com" className="text-white">Instagram</a></li>
+            </ul>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="text-center py-3">
+            <p>&copy; {new Date().getFullYear()} Restaurant Listing. All rights reserved.</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
